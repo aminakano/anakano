bcbff3e1c61318aa052c947a0c62912790f54922
Ami Nakano <aminakano@Amis-MacBook-Air.local>
2018-12-05 23:09:43 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+$(function(){
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+  $('#close-modal').click(function(){
+    $('#signup-modal').fadeOut();
+  });
 
+  $('.lesson').hover(function(){
+    $(this).find('.text-contents').addClass('text-active');
+  },
+  function(){
+    $(this).find('.text-contents').removeClass('text-active');
+  });
+
+  $('.faq-list-item').click(function(){
+
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')){
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    }else {
+      $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
+    }
+
+  })
+})
